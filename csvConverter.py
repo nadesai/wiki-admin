8abdec7d44cbbbfe79ff5f9e4b29d00ba4a063e0
Nikhil Desai <nikhilarundesai@gmail.com>
2014-12-17 17:43:15 -0800

    use pandas for more robust parsing

--- csvConverter.py
+++ csvConverter.py
@@ -1,38 +1,54 @@
+import pandas as pd
+
 location = 'wikiElec.ElecBs3.txt'
+output = 'wikiElec.csv'
 
-data = open(location, encoding='utf8', errors='ignore')
-output = open('wikiElec.ElecBs3.csv', 'w', errors='ignore')
+data = open(location)
 
-result = 0
-date = ""
-time = ""
-candidateID = ""
-candidate = ""
-nominatorID = ""
-nominator = ""
+columns = ['result',
+ 'nomination_date',
+ 'nomination_time',
+ 'candidate_id',
+ 'candidate_user',
+ 'nominator_id',
+ 'nominator_user',
+ 'vote',
+ 'voter_id',
+ 'voter_user',
+ 'vote_date',
+ 'vote_time']
+
+row = {}
+elec = pd.DataFrame(columns=columns)
 
 for line in data:
 	words = line.split()
-	#print(words)
 	if len(words) == 0:
 		continue
-	if words[0] == "E":
-		result = words[1]
-	if words[0] == "T":
-		date = words[1]
-		time = words[2]
-	if words[0] == "U":
-		candidateID = words[1]
-		candidate = words[2]
-	if words[0] == "N":
-		nominatorID = words[1]
-		nominator = words[2]
-	if words[0] == "V":
-		csv = result + "|| " + date + "|| " + time + "|| " + candidateID + "|| " + candidate + "|| " + nominator + "|| " + nominatorID
-		for word in words[1:]:
-			csv = csv + "|| " + word
-		csv = csv + "\n"
-		output.write(csv)
-	
-	
+        if words[0] == '#':
+                continue
+	if words[0] == 'E':
+                #print(words)
+                row['result'] = words[1]
+	if words[0] == 'T':
+                #print(words)
+                row['nomination_date'] = words[1]
+                row['nomination_time'] = words[2]
+	if words[0] == 'U':
+                #print(words)
+		row['candidate_id'] = words[1]
+		row['candidate_user'] = words[2]
+	if words[0] == 'N':
+                #print(words)
+		row['nominator_id'] = words[1]
+		row['nominator_user'] = words[2]
+	if words[0] == 'V':
+                row['vote'] = words[1]
+                row['voter_id'] = words[2]
+                row['vote_date'] = words[3]
+                row['vote_time'] = words[4]
+                row['voter_user'] = words[5]
+                
+                elec = elec.append([row])
 
+elec.to_csv(output,index=False)
